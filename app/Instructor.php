1877cb02f56242fcaa174c51303512c3f79e520f
marcinrekle <marcinrekle@gmail.com>
2016-02-15 14:28:55 +0100

    add Relations to OtO Users, MtM Students

--- app/Instructor.php
+++ app/Instructor.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Instructor extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id'];
+
+    public function user()
+    {
+    	return $this->hasOne('App\User');
+    }
+
+    public function students()
+    {
+    	return $this->hasMany('App\Student');
+    }
+
 }
