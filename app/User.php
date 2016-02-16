103d248afd3ddbf7ee821277e60e17d58f11d87d
marcinrekle <marcinrekle@gmail.com>
2016-02-16 14:05:59 +0100

    Relationship with User Table error

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
 
     public function instructor()
     {
-        return $this->belongsTo('App\Instructor');
+        return $this->hasOne('App\Instructor');
     }
 
 }
