428ca8cb226bc339173c708d6f20166eb3c51804
marcinrekle <marcinrekle@gmail.com>
2016-02-16 14:13:11 +0100

    Create hours view

---
+++ resources/views/admin/student/hours/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">{{$student->user->name}} - dodawanie jazd</h3>
+          </div>
+          <div class="panel-body">
+              
+            {!! Form::open( [
+                  'route' => ['admin.student.hours.store', $student->id]
+              ]) !!}
+              
+              @include('admin.student.hours._form')
+              
+            {!! Form::close() !!}
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@stop
