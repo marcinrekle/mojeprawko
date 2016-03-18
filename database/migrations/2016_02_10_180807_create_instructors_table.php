455297a656f20507fee1485869633250942d3939
marcinrekle <marcinrekle@gmail.com>
2016-03-18 18:38:42 +0100

    add field status

--- database/migrations/2016_02_10_180807_create_instructors_table.php
+++ database/migrations/2016_02_10_180807_create_instructors_table.php
@@ -16,6 +16,7 @@ class CreateInstructorsTable extends Migration
             $table->increments('id')->unsigned();
             $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->string('status',16)->default('active');
             $table->timestamps();
         });
     }
