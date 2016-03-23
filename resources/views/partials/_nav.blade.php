da6170f438edad8e5ee03ea37b11970816df75d0
marcinrekle <marcinrekle@gmail.com>
2016-03-23 13:46:00 +0100

    clean links, some visible only for admin

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -13,8 +13,9 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
+                @if(auth::user()->is_admin)
                 <li class="{{ (Request::is('/admin') ? 'active' : '') }}">
-                    <a href="{{ URL::to('admin') }}"><i class="fa fa-home"></i> Admin</a>
+                    <a href="{{ URL::to('admin') }}"><i class="fa fa-home fa-lg"></i> Admin</a>
                 </li>
                 <li class="{{ (Request::is('admin/student') ? 'active' : '') }}">
                     <a href="{{ URL::to('admin/student') }}">Kursanci</a>
@@ -22,6 +23,11 @@
                 <li class="{{ (Request::is('admin/instructor') ? 'active' : '') }}">
                     <a href="{{ URL::to('admin/instructor') }}">Instruktorzy</a>
                 </li>
+                @else
+                <li class="{{ (Request::is('/student') ? 'active' : '') }}">
+                    <a href="{{ URL::to('student') }}"><i class="fa fa-home fa-lg"></i> </a>
+                </li>
+                @endif
                 <li class="{{ (Request::is('pomoc') ? 'active' : '') }}">
                     <a href="{{ URL::to('pomoc') }}">Pomoc</a>
                 </li>
@@ -34,24 +40,22 @@
                 @if (Auth::guest())
                     <li class="{{ (Request::is('auth/login') ? 'active' : '') }}"><a href="{{ URL::to('auth/login') }}"><i
                                     class="fa fa-sign-in"></i> Zaloguj</a></li>
-                    <li class="{{ (Request::is('auth/register') ? 'active' : '') }}"><a
-                                href="{{ URL::to('auth/register') }}">Zarejestruj</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                           aria-expanded="false"><i class="fa fa-user"></i><img src="{{ Auth::user()->avatar }}" width="30px" height="30px" /> {{ Auth::user()->name }} <i
+                           aria-expanded="false"><img src="{{ Auth::user()->avatar }}" width="30px" height="30px" /> {{ Auth::user()->name }} <i
                                     class="fa fa-caret-down"></i></a>
                         <ul class="dropdown-menu" role="menu">
                             @if(Auth::check())
                                 @if(Auth::user()->is_admin==1)
                                     <li>
-                                        <a href="{{ URL::to('admin/dashboard') }}"><i class="fa fa-tachometer"></i> Admin Dashboard</a>
+                                        <a href="{{ URL::to('admin/dashboard') }}"><i class="fa fa-tachometer"></i> Panel Administratora</a>
                                     </li>
                                 @endif
                                 <li role="presentation" class="divider"></li>
                             @endif
                             <li>
-                                <a href="{{ URL::to('auth/logout') }}"><i class="fa fa-sign-out"></i> Logout</a>
+                                <a href="{{ URL::to('auth/logout') }}"><i class="fa fa-sign-out"></i> Wyloguj</a>
                             </li>
                         </ul>
                     </li>
