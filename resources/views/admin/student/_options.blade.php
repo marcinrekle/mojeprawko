a564a6784b4111104aed3787e089f8c0560243eb
marcinrekle <marcinrekle@gmail.com>
2016-03-23 13:43:57 +0100

    Change add drive link

--- resources/views/admin/student/_options.blade.php
+++ resources/views/admin/student/_options.blade.php
@@ -7,5 +7,5 @@
       <a class="delete" href="#"><i class="fa fa-trash-o fa-2x text-danger"></i></a>
     </div>
 {!! Form::close() !!}
-<a href="{{route('admin.student.hours.create',$student->id)}}" class="" title="Dodaj jazdę"><i class="fa fa-car fa-2x"></i></a>
+<a href="{{route('admin.student.show',$student->id)}}#reservationPanel" class="" title="Dodaj jazdę"><i class="fa fa-car fa-2x"></i></a>
 <a href="{{route('admin.student.payments.create',$student->id)}}" title="Dodaj wpłatę"><i class="fa fa-money fa-2x"></i></a>
