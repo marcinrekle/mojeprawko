f65ade38a58b52255ee3bba539441015f616297a
marcinrekle <marcinrekle@gmail.com>
2016-03-23 13:46:52 +0100

    create Admin Middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if ( !Auth::user()->is_admin ) {
+            return response('Unauthorized.', 401);
+        }
+        return $next($request);
+    }
+}
